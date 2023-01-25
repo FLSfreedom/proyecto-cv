5eefce1a107bece47f749dfaaaace12bc4095cad
Franco <francosalvadeo1@gmail.com>
2023-01-25 01:36:47 -0300

    se completan casi todas las secciones, sólo faltan los links del navbar y la seccion contacto

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,10 @@
 let masInformacion = document.getElementById('masInformacion');
-let infoCoder = document.getElementById('infoCoder', 'infoCoder2');
-let infoUCASAL = document.getElementById('infoUCASAL', 'infoUCASAL2');
-let infoIFTS = document.getElementById('infoIFTS', 'infoIFTS2');
+let infoCoder = document.getElementById('infoCoder');
+let infoUCASAL = document.getElementById('infoUCASAL');
+let infoIFTS = document.getElementById('infoIFTS');
+let infoArgProg1 = document.getElementById('infoArgProg1');
+let infoArgProg2 = document.getElementById('infoArgProg2');
+let infoEdutin = document.getElementById('infoEdutin');
 let divCierre = document.getElementById('divCierre');
 
 function cierreYEliminacion () {
@@ -9,13 +12,22 @@ function cierreYEliminacion () {
 };
 
 infoCoder.addEventListener('click', (e) => {e.preventDefault();
-    [cierreYEliminacion(), masInformacion.innerHTML += '<h4>Coderhouse</h4><p>Desarrollador Front End React<br>6 materias aprobadas<br>Carrera finalizada, certificación obtenida</p>'];
+    [cierreYEliminacion(), masInformacion.innerHTML += '<h4>Coderhouse</h4><p>Desarrollador Front End React<br>HTML5, CSS3 & Javascript. Bootstrap. React. Sass.<br>Carrera finalizada, certificación obtenida.</p>'];
 });
 infoUCASAL.addEventListener('click', (e) => {e.preventDefault();
-    [cierreYEliminacion(), masInformacion.innerHTML += "<h4>Universidad Católica de Salta</h4><p>Abogacía<br>Iniciada en 2019, actualmente cursando el 4o año<br>En curso</p>"];
+    [cierreYEliminacion(), masInformacion.innerHTML += "<h4>Universidad Católica de Salta</h4><p>Abogacía<br>Iniciada en 2019, actualmente cursando el 4to (cuarto) año de carrera.<br>En curso.</p>"];
 });
 infoIFTS.addEventListener('click', (e) => {e.preventDefault();
-    [cierreYEliminacion(), masInformacion.innerHTML += "<h4>IFTS N°16 GCBA</h4><p>Tecnicatura Superior en Análisis de Sistemas<br>6 materias aprobadas<br>En curso</p>"];
+    [cierreYEliminacion(), masInformacion.innerHTML += "<h4>IFTS N°16 GCBA</h4><p>Tecnicatura Superior en Análisis de Sistemas<br>6 materias aprobadas.<br>En curso.</p>"];
+});
+infoArgProg1.addEventListener('click', (e) => {e.preventDefault();
+    [cierreYEliminacion(), masInformacion.innerHTML += '<h4>Argentina Programa "Sé programar"</h4><p>Introducción a la programación orientada a objetos<br>Lenguajes Javascript y Ruby. Septiembre a noviembre de 2022.<br>Certificación obtenida.</p>'];
+});
+infoArgProg2.addEventListener('click', (e) => {e.preventDefault();
+    [cierreYEliminacion(), masInformacion.innerHTML += '<h4>Argentina Programa "Primeros pasos del desarrollo Front End" (Ticmas)</h4><p>Certificación en Desarrollo web Front End<br>HTML5, CSS3 & Javascript. Bootstrap. Iniciado en noviembre de 2022.<br>En curso.</p>'];
+});
+infoEdutin.addEventListener('click', (e) => {e.preventDefault();
+    [cierreYEliminacion(), masInformacion.innerHTML += '<h4>Edutin</h4><p>Curso de certificación en Ruby on Rails<br>Iniciado en diciembre de 2022.<br>En curso.</p>'];
 });
 divCierre.addEventListener('click', (e) => {e.preventDefault();
     [divCierre.style.display = 'none', masInformacion.innerHTML = ''];
